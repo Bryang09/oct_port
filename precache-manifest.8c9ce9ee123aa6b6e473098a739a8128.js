14617fdb3b55650f36e15f0352f756e572c42ba2
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-10-22 18:55:39 -0500

    Updates

---
+++ precache-manifest.8c9ce9ee123aa6b6e473098a739a8128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44a2680d0dd871a46866",
+    "url": "/oct_port/static/js/runtime~main.44a2680d.js"
+  },
+  {
+    "revision": "ee4144152f2ea25be3e9",
+    "url": "/oct_port/static/js/main.ee414415.chunk.js"
+  },
+  {
+    "revision": "acbec2a777a1bf8be9af",
+    "url": "/oct_port/static/js/1.acbec2a7.chunk.js"
+  },
+  {
+    "revision": "ee4144152f2ea25be3e9",
+    "url": "/oct_port/static/css/main.1c027adc.chunk.css"
+  },
+  {
+    "revision": "3cd1ea230eb4a438e1f118dab438a4d2",
+    "url": "/oct_port/index.html"
+  }
+];
